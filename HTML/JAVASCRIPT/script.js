d3a3721a9acc2cdb5957e7701cc5bac4ff33da77
Harjorkhey <masudsummayyah@gmail.com>
2023-12-12 02:47:12 +0100

    HTML/CSS/JAVASCRIPT Popup

---
+++ HTML/JAVASCRIPT/script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById('popup')
+
+function openPopup(){
+    popup.classList.add('0pen-popup')
+}
+
+function closePopup(){
+    popup.classList.remove('open-popup')
+}
